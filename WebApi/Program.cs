e9c258cb07bcfbcd9dd0498ca51531a865a054ab
AlirezaNoor <alirezang7575@gmail.com>
2025-03-06 15:13:54 +0330

    ytytyty

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,37 +1,39 @@
-var builder = WebApplication.CreateBuilder(args);
+using WebAPI.Common.Middlewares;
+using WebAPI.DI;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddBackEndServices(builder.Configuration);
+//>>> Create Logs folder for Serilog
+var logPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "app_data", "logs");
+if (!Directory.Exists(logPath))
+{
+    Directory.CreateDirectory(logPath);
+}
+
+builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.RegisterBackEndBuilder(app.Environment, app, builder.Configuration);
+
+if (!app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
+app.UseRouting();
+app.UseCors();
+app.UseMiddleware<GlobalApiExceptionHandlerMiddleware>();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapStaticAssets();
 
 app.Run();
 
